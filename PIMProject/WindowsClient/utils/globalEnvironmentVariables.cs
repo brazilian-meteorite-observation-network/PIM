8b4e6710510747db308aa233986bf5c1366f3d82
Lucas Bittencourt <lucasnaja0@gmail.com>
2020-01-21 10:26:01 -0300

    add static class to access environment variables

---
+++ PIMProject/WindowsClient/utils/globalEnvironmentVariables.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsClient.utils
+{
+    public static class GlobalEnvironmentVariables
+    {
+        public static void Set(string variable, string value)
+        {
+            Environment.SetEnvironmentVariable(variable, value, EnvironmentVariableTarget.Machine);
+        }
+
+        public static string Get(string variable)
+        {
+            return Environment.GetEnvironmentVariable(variable, EnvironmentVariableTarget.Machine);
+        }
+
+        public static void Remove(string variable)
+        {
+            Environment.SetEnvironmentVariable(variable, null, EnvironmentVariableTarget.Machine);
+        }
+    }
+}
