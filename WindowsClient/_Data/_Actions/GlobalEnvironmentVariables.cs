02b649d08b7e8dbd07c86065820dff1c9dbc5321
Lucas Bittencourt <lucasnaja0@gmail.com>
2020-01-22 23:02:30 -0300

    add comments params and update 'Set' method

--- WindowsClient/_Data/_Actions/GlobalEnvironmentVariables.cs
+++ WindowsClient/_Data/_Actions/GlobalEnvironmentVariables.cs
@@ -4,11 +4,23 @@ namespace WindowsClient._Data._Actions
 {
     public static class GlobalEnvironmentVariables
     {
-        public static void Set(string variable, string h1, float latitude, float longitude)
+        /// <summary>
+        /// Method to set or update a new environment variable to communicate between softwares.
+        /// </summary>
+        /// <param name="variable">Environment variable name</param>
+        /// <param name="latitude">Object's latitude</param>
+        /// <param name="longitude">Object's longitude</param>
+        /// <param name="height">Object's height</param>
+        public static void Set(string variable, float latitude, float longitude, string height)
         {
-            Environment.SetEnvironmentVariable(variable, $"{h1},{latitude},{longitude}", EnvironmentVariableTarget.Machine);
+            Environment.SetEnvironmentVariable(variable, $"{latitude},{longitude},{height}", EnvironmentVariableTarget.Machine);
         }
 
+        /// <summary>
+        /// Method to get the value from the environment variable.
+        /// </summary>
+        /// <param name="variable">Environment variable name</param>
+        /// <returns></returns>
         public static string Get(string variable)
         {
             return Environment.GetEnvironmentVariable(variable, EnvironmentVariableTarget.Machine);
