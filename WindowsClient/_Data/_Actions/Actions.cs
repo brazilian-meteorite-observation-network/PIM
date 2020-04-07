bfe6ba427d494e63fcb33fbad139688080821b4a
BrewertonSantos <brewerton.santos@gmail.com>
2020-04-06 21:03:46 -0300

    improved parameters in Compile Method

--- WindowsClient/_Data/_Actions/Actions.cs
+++ WindowsClient/_Data/_Actions/Actions.cs
@@ -14,18 +14,29 @@ namespace WindowsClient._Data._Actions
     {
         public static string readText { get; set; }
         public static Process process = new Process();
+        public static string PIMFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), ".PIM");
         public static ProcessStartInfo Info = new ProcessStartInfo();
 
         public static void ShowFileContent(string fileName)
         {
-            string folder = Path.Combine(Environment.GetFolderPath(
-    Environment.SpecialFolder.ApplicationData), ".PIM");
+            // to %AppData% + foldername
+            string folder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), ".PIM");
             // Filename  
             string fullPath = folder + "/" + fileName;
             // Read the entire file.
             readText = File.ReadAllText(fullPath);
             FileViewer Content = new FileViewer();
             Content.Show();
+        }
+
+        public static void Compile(string directory, string compiler, string inputfilename, string outputfilename, string format)
+        {
+            Info.WindowStyle = ProcessWindowStyle.Hidden;
+            Info.FileName = "cmd.exe";
+            // if don't use [/C] don't work, u need add a directory string and input in Info.Arguments.
+            Info.Arguments = $"/C cd {directory} & {compiler} {inputfilename}.{format} -o {outputfilename}.{format} & {outputfilename}.{format}";
+            process.StartInfo = Info;
+            process.Start();
         }
     }
 }
