72974f3fdeea309c74872a17243453755b73fd75
BrewertonSantos <brewerton.santos@gmail.com>
2020-04-06 21:06:23 -0300

    Only Method Class in task list

--- WindowsClient/_Data/_Actions/PromptEvents.cs
+++ WindowsClient/_Data/_Actions/PromptEvents.cs
@@ -12,12 +12,13 @@ namespace WindowsClient._Data._Actions
         public static Process process = new Process();
         public static ProcessStartInfo Info = new ProcessStartInfo();
 
+        // [Method.Compile] - Use uses compiler previously defined by the software to compile a specific file.
         public static void Compile(string directory, string compiler, string inputfilename, string outputfilename, string format)
         {
             Info.WindowStyle = ProcessWindowStyle.Hidden;
-            Info.FileName = "cmd.exe";
+            Info.FileName = $"cmd.exe";
             // if don't use [/C] don't work, u need add a directory string and input in Info.Arguments.
-            Info.Arguments = $"/C cd c:/Users/BREWERTONTHIAGOOLIVE/Desktop & {compiler} {inputfilename}.{format} -o {outputfilename}.{format}";
+            Info.Arguments = $"/C cd {directory} & {compiler} {inputfilename}.{format} -o {outputfilename}.{format}";
             process.StartInfo = Info;
             process.Start();
         }
