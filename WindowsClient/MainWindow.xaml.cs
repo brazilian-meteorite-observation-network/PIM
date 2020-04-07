43e904607735f6fe788e465e55539d6ff9be5b8d
BrewertonSantos <brewerton.santos@gmail.com>
2020-04-06 21:01:13 -0300

    Created method to check and order fortran files

--- WindowsClient/MainWindow.xaml.cs
+++ WindowsClient/MainWindow.xaml.cs
@@ -15,13 +15,15 @@ using System.Windows.Shapes;
 using System.Threading;
 using System.ComponentModel;
 using WindowsClient._Data;
+using WindowsClient._Data._Actions;
 using WindowsClient._Data._Helpers;
 using System.Data;
+using System.IO;
 
 namespace WindowsClient
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    ///     All actions to verify this aplication integrity.
     /// </summary>
     public partial class MainWindow : Window
     {
@@ -30,18 +32,19 @@ namespace WindowsClient
             InitializeComponent();
             SQLite.CreateSQLiteDB();
             SQLite.CreateSQLiteTable();
+
+            DirectoryChecker();
             
             SetBackgroundWorker
             (
-                (object sender, DoWorkEventArgs e) => { Thread.Sleep(4000); }, // Freezes the main thread for 5 seconds.
+                (object sender, DoWorkEventArgs e) => { Thread.Sleep(2000); }, // Freezes the main thread for x seconds.
                 (object sender, RunWorkerCompletedEventArgs e) =>
                 {
+                    // Opens the Default form after x seconds.
                     Default frmDefault = new Default();
-
                     frmDefault.Show();
-
                     this.Close();
-                } // Opens the Default form after 5 seconds.
+                }
             );
         }
 
@@ -53,11 +56,24 @@ namespace WindowsClient
         private void SetBackgroundWorker(DoWorkEventHandler DoWork, RunWorkerCompletedEventHandler RunWorkerCompleted)
         {
             BackgroundWorker worker = new BackgroundWorker();
-
             worker.DoWork += DoWork;
             worker.RunWorkerCompleted += RunWorkerCompleted;
-
             worker.RunWorkerAsync();
+        }
+
+        // [ToDo.MoveToOwnClass] - temporarily this directory is here.
+        private void DirectoryChecker()
+        {
+            bool directory = Directory.Exists(@"C:\program files\PIM");
+            bool model3d = File.Exists(@"C:\program files\PIM\model3d_newdens.for");
+            bool kepler2 = File.Exists(@"C:\program files\PIM\kepler2.for");
+
+            if (directory != true)
+                Directory.CreateDirectory(@"C:\program files\PIM");
+            if (model3d != true)
+                File.Copy(Actions.PIMFolder+ "/Model3d_newdens.for", @"C:\program files\PIM\model3d_newdens.for");
+            if (kepler2 != true)
+                File.Copy(Actions.PIMFolder + "/kepler2.for", @"C:\program files\PIM\kepler2.for");
         }
     }
 }
