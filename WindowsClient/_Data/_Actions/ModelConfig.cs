e1b27f2d63dd510861334bbf9247522de46f33df
BrewertonSantos <brewerton.santos@gmail.com>
2020-05-18 08:31:56 -0300

    moved Compile method to PromptEvents.cs

--- WindowsClient/_Data/_Actions/ModelConfig.cs
+++ WindowsClient/_Data/_Actions/ModelConfig.cs
@@ -14,9 +14,9 @@ namespace WindowsClient._Data._Actions
         public static Process process = new Process();
         public static ProcessStartInfo Info = new ProcessStartInfo();
 
-        public static void InsertAllInputs(float lat1, float lat2, float time)
+        public static void InsertAllInputs(float iLat, float iLong, float iHeight, float fLat, float fLong, float fHeight, float time, float dens, float aeroDinCo, float mass, string tab)
         {
-            string folder = "C:/Users/BREWERTONTHIAGOOLIVE/Desktop";//_Data._Actions.DirectoryDialog.Path();
+            string folder = DirectoryDialog.defaultRepo;
             // Filename  
             string fileName = "Model3d_newdens.for";
             string fullPath = folder + "/" + fileName;
@@ -30,34 +30,21 @@ namespace WindowsClient._Data._Actions
             //string readText = File.ReadAllText(fullPath);
             // Read all file lines.
             string[] readLines = File.ReadAllLines(fullPath);
-            // edit time var.
-            readLines[119 - 1] = (/*densidade g/cm3)*/"");
-            readLines[120 - 1] = (/*massa g( M =)  */"");
-            readLines[127 - 1] = (/* CD coeficiente aerodinâmico(CD =) */"");
-            readLines[140 - 1] = (/* tempo entre as capturas segundos(T=) */"");
-            readLines[144 - 1] = (/* altura 1 km (h1=) */"");
-            readLines[145 - 1] = (/* longitude 1 em graus (lon1=) */"");
-            readLines[146 - 1] = (/* latitude 1 em graus(lat1=) */"");
-            readLines[148 - 1] = (/* longitude 1 em graus(lon1=) */"");
-            readLines[149 - 1] = (/* longitude 1 em graus(lon1=) */"");
-            readLines[150 - 1] = (/* latitude em graus(lat1=) */"");
-            readLines[387 - 1] = (/* densidade g/cm3 (densiMeteor=)*/"");
-            readLines[388 - 1] = (/* massa kg(M=) */"");
-            readLines[396 - 1] = (/* CD coeficiente aerodinâmico */"");
-            readLines[140 - 1] = ("	T = " + time + "d0");
-            readLines[141 - 1] = (" Teste");
-            // here -- readLines[14]
+            readLines[119 - 1] = $"            densiMeteor={dens}"; // observation object density
+            readLines[120 - 1] = $" 	    M = {mass}.d-3"; // observation object mass
+            readLines[127 - 1] = $"	    CD = {aeroDinCo}D0"; // aerodynamic coefficient
+            readLines[140 - 1] = $"      T = {time}d0"; // time between initial and final catches
+            readLines[144 - 1] = $"        H1={iHeight}d0"; // observation object initial height
+            readLines[145 - 1] = $"        LON1= {iLong}d0"; // observation object initial longitude
+            readLines[146 - 1] = $"        LAT1={iLat}d0"; // observation object initial latitude
+            readLines[148 - 1] = $"      H2={fHeight}d0"; // observation object final height
+            readLines[149 - 1] = $"        LON2={fLong}d0"; // observation object final longitude
+            readLines[150 - 1] = $"        LAT2={fLat}d0"; // observation object final latitude
+            readLines[387 - 1] = $"            densiMeteor={dens}"; // observation object density
+            readLines[388 - 1] = $"	    M = {mass}.d-3"; // observation object mass
+            readLines[396 - 1] = $"	    CD = {aeroDinCo}d0"; // aerodynamic coefficient
             File.WriteAllLines(fullPath, readLines);
             MessageBox.Show(readLines[140 - 1]);
-        }
-
-        public static void Compile(string directory, string compiler, string inputfilename, string outputfilename, string format)
-        {            Info.WindowStyle = ProcessWindowStyle.Hidden;
-            Info.FileName = "cmd.exe";
-            // if don't use [/C] don't work, u need add a directory string and input in Info.Arguments.
-            Info.Arguments = $"/C cd c:/Users/BREWERTONTHIAGOOLIVE/Desktop & {compiler} {inputfilename}.{format} -o {outputfilename}.{format}";
-            process.StartInfo = Info;
-            process.Start();
         }
     }
 }
