def00810455640c98a531c5d7310b80744e97997
Brewerton Santos <brewerton.santos@gmail.com>
2021-03-16 23:58:23 -0300

    New project standard.

---
+++ PIMWeb/Controllers/AuthenticationController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PIMWeb.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
